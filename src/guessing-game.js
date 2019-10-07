f797c5b4ad8e8a0bd1d59ad43a5e74d7de171484
Marina Sevkovich <slolkunchik@gmail.com>
2019-10-08 02:53:27 +0300

    feat: add class implementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.name = GuessingGame;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.round((this.max + this.min)/2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
